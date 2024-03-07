b01be7dd86b7d6a6159edc161f7c7e475e9f2ea9
elo <rmandanehvash@gmail.com>
2024-03-07 15:52:29 +0330

    save to csv

--- controllers/content.controller.js
+++ controllers/content.controller.js
@@ -1,3 +1,6 @@
+const csv = require("csv");
+const fs = require("fs")
+
 const withdrawalRegex = /برداشت(\d+,\d+)/;
 const depositRegex = /واریز(\d+[\.,]\d+)/;
 
@@ -6,15 +9,35 @@ const depositRegex = /واریز(\d+[\.,]\d+)/;
 function parseContent(req, res) {
   const content = req.body
   const balanceChange = content.split('\n')[1]
+  const date = content.split('\n')[3]
+  console.log(date)
   const withdrawalMatch = balanceChange.match(withdrawalRegex);
   const depositMatch = balanceChange.match(depositRegex);
   if (withdrawalMatch) {
-    let value = parseInt(withdrawalMatch[1].replace(',', ''));
+    const value = parseInt(withdrawalMatch[1].replace(',', ''));
+    // put the value and withdraw in a csv file
+    writeToCSV(value, "withdraw", date);
     res.end(`${value}, withdraw`);
   } else if (depositMatch) {
     let value = parseFloat(depositMatch[1]);
+    const date = withdrawalMatch[3];
+    writeToCSV(value, "deposit", date);
     res.end(`${value}, deposit`);
   }
+  res.end();
+}
+function writeToCSV(value, transactionType, date) {
+  const headers = ["Value", "Transaction Type", "Date"];
+  const filename = "transactions.csv";
+
+  // Check if file exists
+  if (!fs.existsSync(filename)) {
+    // Write headers
+    fs.writeFileSync(filename, headers.join(",") + "\n");
+  }
+
+  // Append data to CSV file
+  fs.appendFileSync(filename, `${value},${transactionType},${date}\n`);
 }
 
 
